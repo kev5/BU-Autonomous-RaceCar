b62da3f7362678fab399a873a1d00831182e045a
naginichen <brchen@bu.edu>
2018-02-28 16:56:17 -0500

    updated user input with ncurses

--- actuation/userinput.c
+++ actuation/userinput.c
@@ -19,6 +19,7 @@ int main()
        cbreak(); // Allows user to exit
        // echo(); // Echoes commands
        keypad(stdscr, TRUE); // stdscr default window
+       nodelay(stdscr, TRUE);
        printf("use arrow keys to control throttle and steering!\n");
 	
        int fid = shm_open("racecar", O_RDWR, 0666);
@@ -34,37 +35,53 @@ int main()
        }
 
        struct throttle_steer *moveptr = (struct throttle_steer *)ptr;
+       
+       int prev_key = ERR;
+       unsigned long err_cnt = 0;
 
        while(1) {
 	  int c = getch();
-	  switch (c) {
-	  case KEY_UP: // pressing up arrow makes car goes forwards
-	    moveptr->throttle += .1;
-	    printf("%f",moveptr->throttle);
-	    break;
-	  case KEY_DOWN: //makes the car go backwards 
-	    moveptr->throttle -= .1;
-	    printf("%f",moveptr->throttle);
-	    break;
-	  case KEY_RIGHT: // steering servo goes one increment to the right
-	    moveptr->steer += .1;
-	    printf("%f",moveptr->steer);
-	    break;
-	  case KEY_LEFT: //steering servo goes one increment to the left
-	    moveptr->steer -= .1;
-	    printf("%f",moveptr->steer);
-	    break;
-	  }
-	  //}
+		
+		switch (c) {
+			case KEY_UP: // pressing up arrow makes car goes forwards
+				moveptr->throttle = 1;
+				printf("%f\n",moveptr->throttle);
+				prev_key = c;
+				break;
+			case KEY_DOWN: //makes the car go backwards 
+				moveptr->throttle = -1;
+				printf("%f\n",moveptr->throttle);
+				prev_key = c;
+				break;
+			case KEY_RIGHT: // steering servo goes one increment to the right
+				moveptr->steer += .2;
+				printf("%f\n",moveptr->steer);
+				prev_key = c;
+				break;
+			case KEY_LEFT: //steering servo goes one increment to the left
+				moveptr->steer -= .2;
+				printf("%f\n",moveptr->steer);
+				prev_key = c;
+				break;
+		         case ERR:
+			   if (prev_key != ERR && err_cnt < 1000000) {
+			     ++err_cnt;
+			   } else {
+			     moveptr->steer = 0;
+			     moveptr->throttle = 0;
+			     printf("Err\r\n");
+			     prev_key = ERR;
+			     err_cnt = 0;
+			   }
+			   break;
+		}
+	 
 	  
-	  //while(1) {
+      
 	  
-	  // printf("Input throttle target: ");
-	  // scanf("%f", &(moveptr->throttle));
-	}
-
-	//fid = munmap(NULL, SHM_SIZE); //unmaps when finished 
-
-return 0;
+    
+   
+       }
+       return 0;
 
 }
