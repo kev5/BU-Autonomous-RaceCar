3f87a1776f26b2c0d3303110690882025017b63b
chuqiao-liang <35831889+chuqiao-liang@users.noreply.github.com>
2018-03-23 15:06:27 -0400

    Update shared_def.h
    added semaphore function prototypes

--- actuation/shared_def.h
+++ actuation/shared_def.h
@@ -2,8 +2,13 @@
 #define __SHARED_DEF_H__
 
 struct throttle_steer {
-  float throttle;   
-  float steer;
+      float throttle;
+      float steer;
 };
 
+typedef sem_t Semaphore;
+Semaphore * sem_make(int value);
+void semaphore_wait(Semaphore *sem);
+void semaphore_post(Semaphore *sem);
 #endif
+~         
