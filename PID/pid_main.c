90d3ceb1d70f813b6092011920b1e1cc3229ac60
nickarnold97 <nicka97@bu.edu>
2018-03-28 13:14:36 -0400

    Added 2 dimensional functionality to the PID, still need to live test steering parameters.

--- PID/pid_main.c
+++ PID/pid_main.c
@@ -23,21 +23,24 @@
 #define SHM_SIZE 4096
 
 
-struct pid_controller ctrldata;
+struct pid_controller throttle_ctrldata;
+struct pid_controller steering_ctrldata;
 
 pid_ct throttle_controller;
-pid_ct steering_controller; // not initialized yet, waiting to do 1d throttle testing
-float throttle_input = 0, throttle_output = 0, steer_input = 0, steer_output = 0;
+pid_ct steering_controller;
 
+float throttle_input = 0, throttle_output = 0, steer_input = 0, steer_output = 0;
 bool pid_active;
 
-// Initial p, i, d values will be refined by actual live testing.. will research dynamic algo to self-modify
-float kp = 6, ki = 3, kd = 2;
+// Initial p, i, d values will be refined by actual live testing.. will tune these params from live testing
+float throttle_kp = 6, throttle_ki = 3, throttle_kd = 2;
+float steering_kp = 6; steering_ki = 3, steering_kd = 2;
 
-
+float setpoint;
+float setpoint_ang;
 
 int main() {
-	/* Initializing interaction with shared mem (based off looking @ actuation files) */
+	/* Initializing interaction with shared mem (based off loothrottle_king @ actuation files) */
 	sem_t *servo_sem = sem_open(SERVOSEM, 1);
 	sem_t *pos_sem = sem_open(POSITSEM,1);
 	if (servo_sem == SEM_FAILED | pos_sem == SEM_FAILED){
@@ -62,31 +65,37 @@ int main() {
 	struct setpoint *current_dest = (struct setpoint *)pos_ptr;
 
 	float distance = (float) pow(pow(current_dest->x_pos,2) + pow(current_dest->y_pos,2),0.5);
+	float angle = current_dest->angle;
 
 	pid_active = true; // hard-coding to true. eventually some control could switch
 
-	// Initializing PID controller (in 1d for now)
-	throttle_controller = pid_create(&ctrldata, &throttle_input, &throttle_output, &(distance) , kp, ki, kd);
+	setpoint = 0;
+	setpoint_ang = 0;
+
+	// Initializing PID controllers
+	throttle_controller = pid_create(&throttle_ctrldata, &throttle_input, &throttle_output, &setpoint , throttle_kp, throttle_ki, throttle_kd);
+	steering_controller = pid_create(&steering_ctrldata, &steer_input, &steer_output, &setpoint_ang, steering_kp, steering_ki, steering_kd);
 	pid_auto(throttle_controller);
+	pid_auto(steering_controller);
 	pid_change_limits(throttle_controller, -1, 1);
+	pid_change_limits(steering_controller, -1, 1);
 
 	/* Main Loop to be run continuously controlling throttle output on Race-car */
 	while (pid_active) {
 		if (pid_need_compute(throttle_controller)) { // time check to see if needed to update PID
 			sem_wait(servo_sem);
+			distance = (float) pow(pow(current_dest->x_pos,2) + pow(current_dest->y_pos,2),0.5);
 			throttle_input = distance; // function to get current location from sensing
-			// function to get set-point coordinate from sensing
 
 			// Compute new result from PID
 			pid_compute(throttle_controller);
+			pid_compute(steering_controller);
+
 			// Update output throttle to shared memory
-
-			moveptr->steer = 0;
+			moveptr->steer = steer_output;
 			moveptr->throttle = throttle_output;
-
 			sem_post(servo_sem);
 		}
-		printf("%f",throttle_output);
+		printf("Throttle: %f , Steering: %f \n",throttle_output, steer_output);
 	}
 }
-
