d332558d6c3d78d0888f391991d8b1fe23b7a06f
chuqiao-liang <35831889+chuqiao-liang@users.noreply.github.com>
2018-04-21 18:45:50 -0400

    add a star files, shared memory (not compilable yet)

--- path_planning/main.cpp
+++ path_planning/main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <vector>
 #include "AStar.hpp"
 #include "obstacles_coords.hpp"
 #define SHM_SIZE 4096
 //shared memory variables
 
+using namespace std;
 
 int main()
 {
-	int fid = shm_open("obstacles", O_CREATE | 0_RDONLY, 0666);
-	if (fid < 0) {
-	perror("shm_open error");
-	return -1;
-	}
+    int fid = shm_open("obstacles", O_CREAT | O_RDONLY, 0666);
+    if (fid < 0) {
+        perror("shm_open error");
+        return -1;
+    }
 
-	void * ptr = mmap(NULL, SHM_SIZE, PROT_READ, MAP_SHARED, fid, 0);
-	if (ptr == MAP_FAILED) {
-	perror("error with mapping");
-	return -1;
-	}
-	vector<vector<int>> * coordsptr;
+    void * ptr = mmap(NULL, SHM_SIZE, PROT_READ, MAP_SHARED, fid, 0);
+    if (ptr == MAP_FAILED) {
+        perror("error with mapping");
+        return -1;
+    }
+    vector<vector<int>> * coordsptr;
 
-	coordsptr = (vector <vector< <int>> obstacles *) ptr; //obstacles is name of vector in header file, shared memory object
+    coordsptr = (vector <vector< <int>> obstacles *) ptr; //obstacles is name of vector in header file, shared memory object
 
-   	AStar::Generator generator;
-    	generator.setWorldSize({25, 25});
-	vector<int> coord;
-   	// obstacle coordinates from shared memory
-  	for  (int i =0; i <= obstacles.size() ; i++){
-	coord = (*coordsptr).at(i);
-	generator.addCollision(coord);
-}
-	
-
+    AStar::Generator generator;
+    generator.setWorldSize({25, 25});
+    vector<int> coord;
+    // obstacle coordinates from shared memory
+    for  (int i =0; i <= obstacles.size() ; i++){
+        coord = (*coordsptr).at(i);
+        generator.addCollision(coord);
+    }
 
     generator.setHeuristic(AStar::Heuristic::euclidean);
     generator.setDiagonalMovement(true);
@@ -43,5 +46,5 @@ int main()
         std::cout << coordinate.x << " " << coordinate.y << "\n";
     }
 
-close(fid);
+    close(fid);
 }
---
+++ path_planning/obstacles_coords.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+#ifndef __OBSTACLES_COORDS_HPP__
+#define __OBSTACLES_COORDS_HPP__
+
+vector <vector <int>> obstacles;
+
+#endif
